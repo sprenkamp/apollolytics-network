362d7251d8489b64e40d902143e49036c2f1279b
ghis9917 <ghis9917@gmail.com>
2024-12-03 16:36:16 +0100

    Adding progress on narratives search

---
+++ src/narrative_search/narratives.py
@@ -0,0 +1,106 @@
+narratives = {
+    'nazis_in_ukraine': {
+        'languages': ['en', 'ru'],
+        'keywords': {
+            'en': [
+                'Nazis in Ukraine',
+                'Ukrainian Nazis',
+                'Neo-Nazi movements',
+                'Fascist groups in Ukraine',
+                'Eliminate Nazi influence',
+                'Fight against fascism'
+            ],
+            'ru': [
+                'нацисты в Украине',
+                'украинские нацисты',
+                'неонацистские движения',
+                'фашистские группы в Украине',
+                'геноцид нацистов',
+                'ликвидировать нацистское влияние',
+                'бороться с фашизмом'
+            ]
+        },
+        'description': {
+            'en': 'Portrays the presence or resurgence of Nazi elements within Ukraine to justify military actions and demonize the Ukrainian government.',
+            'ru': 'Изображает присутствие или возрождение нацистских элементов в Украине для оправдания военных действий и демонизации украинского правительства.'
+        }
+    },
+    'nato_threat': {
+        'languages': ['en', 'ru'],
+        'keywords': {
+            'en': [
+                'NATO expansion',
+                'NATO encroachment',
+                'Western aggression',
+                'Threat to Russia',
+                'Containment by NATO',
+                'NATO military presence',
+                'NATO forces near Russia',
+                'NATO-supported aggression'
+            ],
+            'ru': [
+                'расширение НАТО',
+                'проникновение НАТО',
+                'западная агрессия',
+                'угроза России',
+                'сдерживание НАТО',
+                'военное присутствие НАТО',
+                'войска НАТО рядом с Россией',
+                'агрессия при поддержке НАТО'
+            ]
+        },
+        'description': {
+            'en': 'Frames NATO’s expansion as a direct threat to Russian sovereignty and security, justifying Russia’s defensive measures.',
+            'ru': 'Представляет расширение НАТО как прямую угрозу суверенитету и безопасности России, оправдывая оборонительные меры России.'
+        }
+    },
+    'bioweapons_us': {
+        'languages': ['en', 'ru'],
+        'keywords': {
+            'en': [
+                'Bioweapons by the US',
+                'US bioweapons',
+                'American biological weapons',
+                'US bio-aggression',
+                'Chemical weapons by USA',
+                'Biological warfare US'
+            ],
+            'ru': [
+                'биологическое оружие США',
+                'американские биологические оружия',
+                'биоагрессия США',
+                'химическое оружие США',
+                'биологическая война США'
+            ]
+        },
+        'description': {
+            'en': 'Spreads false or misleading information regarding the development or use of bioweapons by the United States against Russia or Ukraine.',
+            'ru': 'Распространяет ложную или вводящую в заблуждение информацию о разработке или использовании биологического оружия Соединёнными Штатами против России или Украины.'
+        }
+    },
+    'freeing_russians': {
+        'languages': ['en', 'ru'],
+        'keywords': {
+            'en': [
+                'Freeing Russians',
+                'Liberate Russian speakers',
+                'Rescue Russians',
+                'Protect Russian compatriots',
+                'Emancipate Russians in Ukraine',
+                'Defend Russian homeland'
+            ],
+            'ru': [
+                'освобождение русских',
+                'освободить русскоговорящих',
+                'спасение русских',
+                'защита российских соотечественников',
+                'эманация русских в Украине',
+                'защита русской родины'
+            ]
+        },
+        'description': {
+            'en': 'Encourages the liberation of Russian-speaking populations in Ukraine, portraying Russian military actions as protective and humanitarian efforts.',
+            'ru': 'Поощряет освобождение русскоговорящих населения в Украине, представляя российские военные действия как защитительные и гуманитарные усилия.'
+        }
+    }
+}
